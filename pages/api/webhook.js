8f12c9e56b87a0c2844ceff81509112409171fce
Akstar717 <ekeneobioma717@gmail.com>
2026-09-12 16:33:32 +0100

    Create webhook.js

---
+++ pages/api/webhook.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode === 'subscribe' && token === 'my_verify_token_123') {
+      return res.status(200).send(challenge);
+    }
+    return res.status(403).send('Forbidden');
+  }
+
+  res.status(200).json({ status: 'webhook alive' });
+}
